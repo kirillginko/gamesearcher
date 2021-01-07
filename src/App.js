5f79e93d86bcc17bcaaaf35c785e3f110753f83c
kirill ginko <kirillginko@gmail.com>
2021-01-07 00:34:04 -0500

    fetch data and display upcoming games

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import React from "react";
+//Components
+import Home from "./pages/Home";
+import Game from "./components/Game";
+
 function App() {
   return (
     <div>
-      <h1>Hello Ignite</h1>
+      <Home />
     </div>
   );
 }
